d89f637281b76f1db25487ceed2a22ddaab21dcf
Ferlito <75152820+LeviathanH@users.noreply.github.com>
2021-06-07 13:29:46 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# rdpk
+# RDP Windows 2019 Free By IwassDiee
+
++ Jika Ingin Mengetahui Cara Membuat RDP Gratis
++ Bisa Chat Nomor Di Bawah Ini Atau Klik Link Di Bawah Ini!.
++ >> https://wa.me/62895383075590 >> +
---
+++ start.bat
@@ -0,0 +1,25 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By IwassDiee" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user administrator @ServerMalik /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+echo Nginstal Berhasil, Kalo Mati Rdp Nya Tar Rebuild Lagi Ye!
+echo ===========================
+echo *RESULT OF RDP BY LEVIHS*
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
+echo Username: administrator
+echo Password: @ServerLevi
+echo ===========================
+echo Silahkan Login Ke RDP Lu Sekarang Anjg!
+echo Jangan Lupa Bilang Makasih Njeng Ke Bos Malik 7!
+ping -n 10 127.0.0.1 >nul
